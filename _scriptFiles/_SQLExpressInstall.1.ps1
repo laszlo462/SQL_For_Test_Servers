cf46e2bb1a5c243ebd590281aa0895018a24c240
Steve Szabo <laszlo.steve@gmail.com>
2018-08-15 16:44:14 -0500

    external function calls ready for testing

--- _scriptFiles/_SQLExpressInstall.1.ps1
+++ _scriptFiles/_SQLExpressInstall.1.ps1
@@ -79,6 +79,24 @@ function DotNet3Install
     Write-Host "`n"
 }
 
+function SetSQLMixedMode
+{
+    #### Registry key change to switch SQL to mixed-mode auth after install.
+    #### This is done to prevent insecure passing of /SAPWD parameter during install script arguments.
+    $registryPath = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL11.SQLEXPRESS\MSSQLServer"
+    $name = LoginMode
+    $value = "2"
+
+    if (Test-Path $registryPath)
+    {
+        New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+        Write-Host "Complete!" -ForegroundColor Green
+        Write-Host "`n"
+    }else{
+        Write-Host "Unable to find registry key to set SQL Login mode, please do so manually via GUI after rebooting..." -ForegroundColor Red
+    }
+}
+
 Write-Host ".NET 3.5 Prerequisite Check..."
 $netFX3dir = "C:\Windows\Microsoft.NET\Framework\v3.5"
 $exists = Test-Path -Path $netFX3dir
@@ -87,7 +105,7 @@ if ($exists)
     Write-Host "Success! .NET 3.5 already installed" -ForegroundColor Green
 }else{
     Write-Host ".Net 3.5 not found, performing automatic install..."
-    if (Test-Path -Path ..\..\DotNet3)
+    if (Test-Path -Path ..\..\DotNet3\_scriptFiles)
     {
         Set-Location ..\..\DotNet3
         DotNet3Install
@@ -157,7 +175,23 @@ If ($sqlSetupPath -eq $null -Or $sqlSp3Path -eq $null)
     Write-Host $separator
     Write-Host "SP3 for SQL 2012 installation complete.  Error handling is still WIP!"
     Write-Host "Exit code of 3010 is good"
-    Read-Host "Reboot required, press enter to reboot now..."
-    Restart-Computer
-    exit
+    #Read-Host "Reboot required, press enter to reboot now..."
+    Write-Host $separator
+    Read-Host "SP3 install complete.  Forgoing required reboot for testing purposes."
+
+    Write-Host "Setting SQL Auth to mixed mode..." -ForegroundColor Green
+    SetSQLMixedMode
+
+    Write-Host "Calling external function to enable SA and reset password..."
+    Reset-DbaAdmin -SqlInstance .\SQLEXPRESS
+    Write-Host "`n"
+
+    Write-Host "Configuring correct default TCP ports" -ForegroundColor Green
+    Set-DbaTcpPort -SqlInstance .\SQLEXPRESS -Port 1433
+    Write-Host "`n"
+
+
+    ####Testing#####Turned off reboot####
+    # Restart-Computer
+    # exit
 }
